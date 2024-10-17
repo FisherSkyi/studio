1af5319f440197dd63aabd5f739f37c22ba00706
Source Academy User <No public email provided>
2024-10-17 10:10:11 +0800

    Changes made from Source Academy

---
+++ Ref_09.js
@@ -0,0 +1,23 @@
+// function f(i, n) {
+//     i === n ? i : n + f(i, n+1);
+// }
+
+// f(5,1);
+
+// function f(n) {
+//     return n === 0 ? f(n-1) ? true : true
+//                     : true;
+// }
+
+// f(16);
+
+//Q1
+[20,15,10,5,1];
+
+list(20, 15, 15, 5, 0);
+
+function f(x1, x2, x3, x4, x5) {
+    return 1;
+}
+
+f(20,15,10,5,1);
