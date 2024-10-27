dde3a1e32f23c0f5c461b02095612af5c7c27ef3
Source Academy User <No public email provided>
2024-10-27 21:28:48 +0800

    Changes made from Source Academy

--- 11_selfmade.js
+++ 11_selfmade.js
@@ -1,44 +1,61 @@
 
-function stream_map(f, s) {
-    return is_null(s)
-        ? null
-        : pair(f(head(s)),
-               () => stream_map(f, stream_tail(s)));
+// function stream_map(f, s) {
+//     return is_null(s)
+//         ? null
+//         : pair(f(head(s)),
+//               () => stream_map(f, stream_tail(s)));
+// }
+
+// // () => stream_map(f, scale_stream(2, A))
+// // () => stream_map(f, stream_map(x => 2 * x, A))
+
+// function add_streams(s1, s2) {
+//     return is_null(s1)
+//         ? s2
+//         : is_null(s2)
+//         ? s1
+//         : pair(head(s1) + head(s2),
+//               () => add_streams(stream_tail(s1), 
+//                                  stream_tail(s2)));
+// }
+
+// const ones = pair(1, () => ones);
+
+// const integers = pair(1, () => add_streams(ones, integers));
+
+// function scale_stream(c, stream) {
+//     return stream_map(x => c * x, stream);
+// }
+
+// const A = pair(1, () => scale_stream(3, A));
+
+// function mul_streams(a, b) { 
+//     return pair(head(a) * head(b),
+//                     () => mul_streams(stream_tail(a), stream_tail(b)));
+// }
+
+// const B = pair(1, () => mul_streams(B, integers));
+
+// eval_stream(A, 10);
+
+// eval_stream(B, 10);
+
+
+function integers_from(n) {
+    return pair(n, () => integers_from(n + 1));
+}
+const integers2 = integers_from(1);
+eval_stream(integers2, 10);
+
+function alt_stream(n) {
+    return pair(n % 2 === 1? 1 : -1, () => alt_stream(n + 1));
 }
 
-// () => stream_map(f, scale_stream(2, A))
-// () => stream_map(f, stream_map(x => 2 * x, A))
+const alternating_ones = alt_stream(1);
 
-function add_streams(s1, s2) {
-    return is_null(s1)
-        ? s2
-        : is_null(s2)
-        ? s1
-        : pair(head(s1) + head(s2),
-               () => add_streams(stream_tail(s1), 
-                                 stream_tail(s2)));
-}
+eval_stream(alternating_ones, 10);
 
-const ones = pair(1, () => ones);
-
-const integers = pair(1, () => add_streams(ones, integers));
-
-function scale_stream(c, stream) {
-    return stream_map(x => c * x, stream);
-}
-
-const A = pair(1, () => scale_stream(3, A));
-
-function mul_streams(a, b) { 
-    return pair(head(a) * head(b),
-                    () => mul_streams(stream_tail(a), stream_tail(b)));
-}
-
-const B = pair(1, () => mul_streams(B, integers));
-
-eval_stream(A, 10);
-
-eval_stream(B, 10);
+function alt_ones1()
 
 
 
